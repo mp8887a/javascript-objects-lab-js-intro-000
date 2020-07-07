943eec7c08ae2e327de868c1ea0e97a45a426f96
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-07 16:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var recipes = new Object();
 
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},objet,{[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  rturn newObj
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
